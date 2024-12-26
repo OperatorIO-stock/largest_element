668874bddae0c26c982e9a4aaf7706b63f84abd9
OperatorIO-stock <vova.chigorin@yandex.com>
2024-12-26 13:55:41 +0300

    commit G

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+
+namespace largest_element;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int rows = 10;
+        int cols = 10;
+
+        int[,] array = new int[rows, cols];
+
+        Random random = new Random();
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                array[i, j] = random.Next(1, 10);
+
+        int maxElement = array[0, 0];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+                if (array[i, j] > maxElement)
+                    maxElement = array[i, j];
+        }
+
+        Console.WriteLine($"Max number of the matrix: {maxElement}\n");
+        Console.WriteLine("The original version of the matrix");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+                Console.Write($"{array[i, j]} ");
+            
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\nThe resulting matrix");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (array[i, j] == maxElement)
+                    array [i, j] = 0;
+                
+                Console.Write($"{array[i, j]} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
